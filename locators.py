5d6b98107c053f373f5900fb3934443dea0c2362
CyberClown-png <bkolesnsk0@gmail.com>
2021-06-07 13:06:44 +0300

    add ProductPageLocators

--- locators.py
+++ locators.py
@@ -7,3 +7,7 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR,".btn.btn-lg.btn-primary.btn-add-to-basket")
+
